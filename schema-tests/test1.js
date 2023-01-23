ddc78b22c0b31be64e0bef078e881f18810628bf
Pcjmy <1716045191@qq.com>
2023-01-23 22:15:50 +0800

    json-schema format

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,11 +1,16 @@
 const Ajv = require('ajv')
+const addFormats = require('ajv-formats')
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+addFormats(ajv)
 
 const schema = {
   type: 'object',
   properties: {
     foo: { type: 'integer' },
-    bar: { type: 'string' },
+    bar: {
+      type: 'string',
+      format: 'email',
+    },
   },
   required: ['foo'],
   additionalProperties: false,
@@ -15,8 +20,7 @@ const validate = ajv.compile(schema)
 
 const data = {
   foo: 1,
-  bar: 'abc',
-  age: 18,
+  bar: 'abc@qq.com',
 }
 
 const valid = validate(data)
